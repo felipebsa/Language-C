56ecd4f4e5113b5c924c067716a4611dd484fecb
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-04-17 17:12:19 -0300

    add aula02.c

--- aula01.c
+++ aula01.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
-
 int main()
 {
-    printf("hello world!\nnao gosto da linguagem C");
+    float weight, height, IMC;
+    printf("escreva o peso: ");
+    scanf("%f", &weight);
+    printf("escreva a altura: ");
+    scanf("%f", &height);
+    IMC = weight / (height * height);
+    if (IMC >= 30) {
+        printf("obeso");
+    }
+    else if (IMC >= 25){
+        printf("sobrepeso");
+    }
+    else if (IMC >= 18.5) {
+        printf("normal");
+    }
+    else {
+        printf("abaixo do peso");
+    }
+    printf("\nIMC == %1.f", IMC);
     return 0;
-}
+}   
