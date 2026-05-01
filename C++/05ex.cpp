c7560003308ea83e0b2dc2fda1e4d307df93786f
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-01 14:07:29 -0300

    fix 05ex.cpp question

--- C++/05ex.cpp
+++ C++/05ex.cpp
@@ -1,11 +1,11 @@
 /*
 * Instituição: EtecVAV - Vasco Antonio Vechiarutti
 *
-* Arquivo: 05ex.cpp
-* Data: 26/04/2026
+* Arquivo: 05ex_fixed.cpp
+* Data: 01/05/2026
 * Autor: Felipe Barbosa Santos
-* Descrição: 
-* Ler um número para a parada final e outro número que representa um múltiplo. 
+* Descrição:
+* Ler um número para a parada final e outro número que representa um múltiplo.
 * Exibir os múltiplos do número lido de 1 até o número final lido.
 */
 
@@ -13,6 +13,22 @@
 using namespace std;
 
 int main() {
-    
+    int limit, mult;
+
+    cout << "Enter the multiple base: ";
+    cin >> mult;
+
+    cout << "Enter the stop number: ";
+    cin >> limit;
+
+    cout << "Multiples of " << mult << " from 1 to " << limit << ": " << endl;
+
+    for (int i = 1; i <= limit; i++) {
+        if (i % mult == 0) {
+            cout << i << "  ";
+        }
+    }
+
+    cout << endl;
     return 0;
 }
