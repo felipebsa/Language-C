49a1a98db9466c444065e20c89e5818909a38f0c
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 20:53:01 -0300

    feat: add ex04

--- C++/L2/ex04/main.cpp
+++ C++/L2/ex04/main.cpp
@@ -4,7 +4,61 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-    
+int sum() {
+    int n1, n2, sum = 0;
+    cin >> n1;
+    cin >> n2;
+    sum = n1 + n2;
+    cout << sum << endl;
     return 0;
+}
+
+int sub() {
+    int n1, n2, sub = 0;
+    cin >> n1;
+    cin >> n2;
+    sub = n1 - n2;
+    cout << sub << endl;
+    return 0;
+}
+
+int multi() {
+    int n1, n2, multi = 0;
+    cin >> n1;
+    cin >> n2;
+    multi = n1 * n2;
+    cout << multi << endl;
+    return 0;
+}
+
+int div() {
+    int n1, n2, div = 0;
+    cin >> n1;
+    cin >> n2;
+    div = n1 / n2;
+    cout << div << endl;
+    return 0;
+}
+
+int main() {
+    char choice;
+    bool power = true;
+    cout << "---- Calculator ----" << endl;
+        cout << "Calculator" << endl << "choice [+] [-] [*] [/] or [exit]" << endl;
+        cin >> choice;
+        if (choice == '+') {
+            sum();
+        }
+        else if (choice == '-') {
+            sub();
+        }
+        else if (choice == '*') {
+            multi();
+        }
+        else if (choice == '/') {
+            div();
+        }
+        else {
+            return 0;
+        }
 }
