fdbb3a143c6f7bc5878070e974cb712f80b2e089
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 21:03:06 -0300

    feat: add ex05

--- C++/L2/ex05/main.cpp
+++ C++/L2/ex05/main.cpp
@@ -4,7 +4,20 @@
 #include <algorithm>
 using namespace std;
 
+void palin() {
+    string word;
+    cin >> word;
+    string word2 = word;
+    reverse(word2.begin(), word2.end());
+    if (word == word2) {
+        cout << "is palindromo";
+    }
+    else {
+        cout << "not is palindromo";
+    }
+
+}
 int main() {
-    
+    palin();
     return 0;
 }
