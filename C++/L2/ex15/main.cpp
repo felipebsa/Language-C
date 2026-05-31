3641d0c9fadbda564c3f47aeb550e2c33d19efd8
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 23:09:13 -0300

    feat: add ex15

--- C++/L2/ex15/main.cpp
+++ C++/L2/ex15/main.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 
 int main() {
-    int n = 10; 
+    int n = 20; 
     vector<int> v(n);
     
     for (int i = 0; i < n; i++) cin >> v[i];
     
+    for (int i = 0; i < n; i++) {
+        if(v[i] < 0) {cout << i; break;}
+    }
     return 0;
 }
