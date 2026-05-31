d35761c6b00eac71081ba99e5c256546234828e0
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-05-30 21:26:10 -0300

    feat: add ex07

--- C++/L2/ex07/main.cpp
+++ C++/L2/ex07/main.cpp
@@ -4,7 +4,22 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+void order() {
+    vector<int> n(5);
+    bool c = true, d = true;
+    for (int i = 0; i < 5; i++) {
+        cin >> n[i];
+    }
+    for (int i = 0; i < 4; i++) {
+        if (n[i] > n[i+1]) c = false;
+        else d = false;
+    }
+    if (c) cout << "crescente";
+    else if (d) cout << "descrescente";
+    else cout << "nao ordenado";
     
+}
+int main() {
+    order();
     return 0;
 }
